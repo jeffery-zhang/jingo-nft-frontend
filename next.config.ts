d11bfc8f9b659dfc3ca307a25133d60e2bb62fa7
jeffery-zhang <jefferyzhang1013@gmail.com>
2025-11-07 17:18:26 +0800

    add next-on-pages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from 'next'
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform()
+}
 
 const nextConfig: NextConfig = {
+  // cloudflare compability
+  output: 'standalone',
   /* config options here */
   images: {
     remotePatterns: [new URL('https://fuchsia-absolute-porcupine-16.mypinata.cloud/ipfs/**')],
